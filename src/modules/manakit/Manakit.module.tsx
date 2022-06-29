7a60820a000596651a4ffa89a55bea88ddde1cd6
Laurent Grimaldi <62988176+GrimLaurent@users.noreply.github.com>
2022-06-29 10:46:18 +0200

    Delete demo stories + WIP Manakit Hydratation + Add Documentation for Manakit

--- src/modules/manakit/Manakit.module.tsx
+++ src/modules/manakit/Manakit.module.tsx
@@ -3,12 +3,14 @@ import React, { Fragment, FunctionComponent } from "react";
 import { hydrate } from "ReactDOM";
 import ManakitType from "./types/Manakit.types";
 
-const ManaKit: FunctionComponent<ManakitType> = ({ children, ...props }) => {
-  if (typeof window === "undefined") {
-    return null;
-  } // fix hydratation
+const isSSR = () => typeof window === "undefined";
 
-  return <Fragment>{children}</Fragment>;
+const ManaKit: FunctionComponent<ManakitType> = ({ children }) => {
+  // if (typeof window === "undefined") {
+  //   return null;
+  // } // fix hydratation
+
+  return !isSSR() && children;
 };
 
-export default hydrate(ManaKit);
+export default ManaKit;
