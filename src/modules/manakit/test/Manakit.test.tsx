72e49da6da8d781f9cd21b4479d61c8364fea212
Laurent Grimaldi <62988176+GrimLaurent@users.noreply.github.com>
2022-07-02 12:10:21 +0200

    add package  jest react test

--- src/modules/manakit/test/Manakit.test.tsx
+++ src/modules/manakit/test/Manakit.test.tsx
@@ -1,22 +1,18 @@
 import React from "react";
-import { render, screen } from "@testing-library/react";
+import { render } from "@testing-library/react";
 import Manakit from "../Manakit.module";
-
-// Doc à lire pour la suite https://jestjs.io/fr/docs/tutorial-react
 
 describe("structure Manakit", () => {
   const props = {
-    noControl: false,
     children: `<>
       <div>1</div>
       <div>2</div>
       <div>3</div>
       <div>4</div>
-      <div> Demo </iv>
     </>`,
   };
 
-  test("render default Manakit", () => {
+  test("test import props", () => {
     render(<Manakit {...props} />);
   });
 });
