b98c980edaadb118cf10d7bda565cf884c27e5c7
Laurent Grimaldi <62988176+GrimLaurent@users.noreply.github.com>
2022-07-02 11:54:18 +0200

    add ts for test + fix bug hydratation + clean code

--- src/modules/manakit/Manakit.module.tsx
+++ src/modules/manakit/Manakit.module.tsx
@@ -5,9 +5,12 @@ import ManakitType from './types/Manakit.types';
 const ManaKit: FunctionComponent<ManakitType> = ({ children }) => {
   const [render, setRender] = useState(null) as any;
 
+  // var
+  const id = 'manakit-ui';
+
   useEffect(() => {
     // fix Hydration failed because the initial UI does not match what was rendered on the server
-    setRender(<div>{children}</div>);
+    setRender(<div id={id}>{children}</div>);
   }, []);
 
   return render;
