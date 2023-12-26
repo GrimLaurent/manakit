74825c0c304528d6e57cc7ab39c3cc84c6ad4864
Laurent Grimaldi <laurent.grimaldi@minedelve.com>
2023-12-26 19:51:22 +0100

    create first component Svelte for Manakit

---
+++ src/lib/components/App/App.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+	export let message: String = 'Manakit - Svelte Power';
+</script>
+
+<div data-app="manakit">
+	{message}
+</div>
--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,1 +1,4 @@
 // Reexport your entry components here
+
+// Svelte Components
+export { default as App } from './components/App/App.svelte';
