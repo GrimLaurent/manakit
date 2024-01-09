e4dfe6d1581de26c67eede91e5584cda077af8f7
Laurent Grimaldi <laurent.grimaldi@minedelve.com>
2024-01-09 22:03:12 +0100

    new component toolbar + spacer with next generation manakit standard

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -5,8 +5,8 @@ export { default as Col } from './legacy/Col/Col.svelte';
 export { default as Container } from './legacy/Container/Container.svelte';
 export { default as DividerLegacy } from './legacy/Divider/Divider.svelte';
 export { default as Row } from './legacy/Row/Row.svelte';
-export { default as Spacer } from './legacy/Spacer/Spacer.svelte';
-export { default as Toolbar } from './legacy/Toolbar/Toolbar.svelte';
+export { default as SpacerLegacy } from './legacy/Spacer/Spacer.svelte';
+export { default as ToolbarLegacy } from './legacy/Toolbar/Toolbar.svelte';
 export { default as Responsive } from './legacy/Responsive/Responsive.svelte';
 export { default as Main } from './legacy/Main/Main.svelte';
 export { default as Footer } from './legacy/Footer/Footer.svelte';
@@ -34,5 +34,9 @@ export { default as Alert } from './components/alert/alert.svelte';
 
 export { default as Avatar } from './components/avatar/avatar.svelte';
 export { default as AvatarGroup } from './components/avatar/avatar.group.svelte';
+
+export { default as Toolbar } from './components/toolbar/toolbar.svelte';
+
+export { default as Spacer } from './components/spacer/spacer.svelte';
 
 export { default as CardLegacy } from './legacy/Card/Card.svelte';
